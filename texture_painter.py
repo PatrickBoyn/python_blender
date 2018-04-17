a025c2d9478fb0a155cc0c4c8bfb1cd6bace288b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-17 17:31:25 -0400

    The starting project files.

---
+++ texture_painter.py
@@ -0,0 +1,7 @@
+'''Renders text from a CSV file to textures 
+   and applies them to multiple objects. 
+   Use these when starting blender:
+   import os, sys, importlib; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
+   After running the above:
+   importlib.reload(texture_painter)
+   '''
