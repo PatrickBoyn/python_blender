1224c5c4ef3304734fe2e4f733d07f1e1e4e5838
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-17 18:31:13 -0400

    Returned things the way they were before. Also fixed a comma on my imports.

--- texture_painter.py
+++ texture_painter.py
@@ -1,7 +1,7 @@
 '''Renders text from a CSV file to textures 
    and applies them to multiple objects. 
    Use these when starting blender:
-   import os, sys importlib; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
+   import os, sys, importlib; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
    After running the above:
    importlib.reload(texture_painter)
    '''
@@ -9,8 +9,8 @@ import codecs
 
 
 def get_text(csv_filename):
-    codecs.open(r'C:\Users\dakil\Desktop\Repos\python_blender\\' + csv_filename)
-
+    stream = codecs.open(r'C:\Users\dakil\Desktop\Repos\python_blender\\' + csv_filename)
+    return stream
 def go():
      print("Texture Painter is working!")
-     get_text('crawl.txt')
+     print( get_text('crawl.txt'))
