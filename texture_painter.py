ce868fa2d50c1a317af11767ee2df9a0345dcbbe
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-17 18:18:02 -0400

    Added the function that grabs the text.

--- texture_painter.py
+++ texture_painter.py
@@ -1,7 +1,16 @@
 '''Renders text from a CSV file to textures 
    and applies them to multiple objects. 
    Use these when starting blender:
-   import os, sys, importlib; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
+   import os, sys importlib; sys.path.append(os.path.dirname(bpy.data.filepath)); import texture_painter
    After running the above:
    importlib.reload(texture_painter)
    '''
+import codecs
+
+
+def get_text(csv_filename):
+    text = codecs.open(r'C:\Users\dakil\Desktop\Repos\python_blender' + '\\' + csv_filename)
+
+def go():
+     print("Texture Painter is working!")
+     get_text('crawl.txt')
