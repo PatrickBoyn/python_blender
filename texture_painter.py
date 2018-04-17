85f142efd45c196afb182bf7fb1c5712997259cd
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-04-17 18:34:01 -0400

    A bit of cleaning up and also added a comment.

--- texture_painter.py
+++ texture_painter.py
@@ -7,10 +7,13 @@
    '''
 import codecs
 
-
+# Please note that this only works inside blender. Running this code outside produces None objects.
 def get_text(csv_filename):
     stream = codecs.open(r'C:\Users\dakil\Desktop\Repos\python_blender\\' + csv_filename)
     return stream
+
+
 def go():
      print("Texture Painter is working!")
      print( get_text('crawl.txt'))
+     
